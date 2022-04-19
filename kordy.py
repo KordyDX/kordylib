6c2666b2826098c23b7f4b6f177dbb999b231449
Kristián Stefurak <76947058+KordyDX@users.noreply.github.com>
2022-04-19 19:54:01 +0200

    Add files via upload

---
+++ kordy.py
@@ -0,0 +1,9 @@
+#Standard class
+class Kordy():
+    #init passes to main function
+    def __init__(self):
+        pass
+    #Main function, conversion from lightyears to km by multiplification
+    def lightKm(self, lighty):
+        return lighty * 9460730472580
+    
---
+++ main.py
@@ -0,0 +1,8 @@
+#Import library
+from kordy import Kordy
+#Take input
+k = int(input("Lightyears: "))
+#Call and save class
+object = Kordy()
+#Call class function
+print(object.lightKm(k))
